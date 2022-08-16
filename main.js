bb30459a310507477c228de4ba84bfc016de2da2
Hasanul Bari <hasanulbari8@gmail.com>
2022-08-16 23:25:33 +0600

    final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const p1ScoreDisplay = document.getElementById('p1Score');
 const p2ScoreDispaly =document.getElementById('p2Score');
-const wScoreDisplay = document.querySelector('span');
+const wScoreDisplay = document.querySelector('p span');
 const inputScore = document.getElementById('input-score');
 
 const p1Btn = document.getElementById('p1Btn');
@@ -11,6 +11,32 @@ let p1Score = 0;
 let p2Score = 0;
 let winingScore = 5;
 let gameOver = false;
+
+function winner(oldScore,winningScore){
+    if(oldScore === winningScore){
+        if(p1Score === winningScore){
+            p1ScoreDisplay.classList.add("winner");
+        }
+        else{
+            p2ScoreDispaly.classList.add("winner");
+        }
+        gameOver = true
+         p1Btn.setAttribute('disabled','disabled');
+         p2Btn.setAttribute('disabled','disabled');
+    }
+}
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    gameOver = false;
+    p1ScoreDisplay.innerText =0;
+    p2ScoreDispaly.innerText = 0;
+    p1ScoreDisplay.classList.remove("winner")
+    p2ScoreDispaly.classList.remove("winner")
+    p1Btn.removeAttribute('disabled');
+    p2Btn.removeAttribute('disabled');
+}
 
 p1Btn.addEventListener('click',function(){
    
@@ -29,21 +55,16 @@ p2Btn.addEventListener('click',function(){
     p2ScoreDispaly.innerText = p2Score; 
 })
 
-function winner(oldScore,winningScore){
-    if(oldScore === winningScore){
-        gameOver = true
-         p1Btn.setAttribute('disabled','disabled');
-         p2Btn.setAttribute('disabled','disabled');
-    }
-}
 
-resetBtn.addEventListener('click',function(){
-    p1Score = 0;
-    p2Score = 0;
-    gameOver = false;
-    p1ScoreDisplay.innerText =0;
-    p2ScoreDispaly.innerText = 0;
-    p1Btn.removeAttribute('disabled');
-    p2Btn.removeAttribute('disabled');
+
+inputScore.addEventListener('change',function(){
+  winingScore = parseFloat(inputScore.value);
+  wScoreDisplay.textContent = inputScore.value;
+  inputScore.value = "";
+  reset()
 })
+
+
+
+resetBtn.addEventListener('click',reset)
 
