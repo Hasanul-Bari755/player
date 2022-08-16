cbf3249656e1b2225b1408db76078f83b8430a5a
Hasanul Bari <hasanulbari8@gmail.com>
2022-08-16 21:06:46 +0600

    commit message

--- main.js
+++ main.js
@@ -10,21 +10,29 @@ const resetBtn = document.getElementById('resetBtn');
 let p1Score = 0;
 let p2Score = 0;
 let winingScore = 5;
+let gameOver = false;
 
 p1Btn.addEventListener('click',function(){
-    p1Score++;
-    if(p1Score === winingScore){
-        p1Btn.setAttribute('disabled','disabled');
-        p2Btn.setAttribute('disabled','disabled');
+   
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winingScore){
+            gameOver = true;
+            // p1Btn.setAttribute('disabled','disabled');
+            // p2Btn.setAttribute('disabled','disabled');
+        }
     }
     p1ScoreDisplay.innerText = p1Score; 
 })
 
 p2Btn.addEventListener('click',function(){
-    p2Score++;
+    if(!gameOver){
+        p2Score++;
     if(p2Score === winingScore){
-        p1Btn.setAttribute('disabled','disabled');
-        p2Btn.setAttribute('disabled','disabled');
+        gameOver = true
+        // p1Btn.setAttribute('disabled','disabled');
+        // p2Btn.setAttribute('disabled','disabled');
+    }
     }
     p2ScoreDispaly.innerText = p2Score; 
 })
